693ed1a473e9a9d5285dcc2128d6edb4bf35f361
Jeffrey Bolling <j2b2590@gmail.com>
2020-09-07 20:36:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def nyc_pigeon_organizer(data)
     binding.pry
 
     value.each do |inner_key,  names|
-      names.each do |name|
+      names.collect do |name|
        if !final_array[name]
          final_array[name] = {}
        end
